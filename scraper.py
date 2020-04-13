7c2cff254c2d390f89978e1d26874e2d2b3f18d8
Lee Korff-Korn <leekk270@gmail.com>
2020-04-13 01:45:50 -0400

    created scraper

---
+++ scraper.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys 
+from datetime import datetime
+CHROME_DRIVER = './chromedriver'
+FIREFOX_DRIVER = './geckodriver'
+import time
+import re
+import json
+import random
+
+##starting views are 187, 72, 49
+
+def setup_driver(headless=True, driver_type=CHROME_DRIVER):
+    if 'chrome' in driver_type.lower():
+        options = ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--ignore-ssl-errors')
+
+        if headless:
+            options.add_argument('-headless')
+        driver = webdriver.Chrome(executable_path=driver_type, chrome_options=options)
+    else:
+        options = FirefoxOptions()
+        if headless:
+            options.add_argument('-headless')
+        driver = webdriver.Firefox(executable_path=driver_type, firefox_options=options)
+
+    return driver
+
+
+def mute(driver, url):
+    actions = ActionChains(driver)
+    driver.get(url)
+    time.sleep(4)
+    actions.send_keys("m").perform()
+    time.sleep(4)
+    executing_query = 'document.getElementById("toggleBar").click()'
+    driver.execute_script(executing_query)
+    time.sleep(3)
+  
+
+def get_soup(driver, url):
+    actions = ActionChains(driver)
+    driver.get(url)
+    time.sleep(4)
+    # actions.send_keys(Keys.SPACE).perform()
+    # time.sleep(4)
+    
+
+def run():
+    driver = setup_driver()
+    cont = True
+    urls = ['https://www.youtube.com/watch?v=Hp4xjixyUVA', 'https://www.youtube.com/watch?v=wX5Sj5NAZ4o', 'https://www.youtube.com/watch?v=M9gqND4R6uU&t=268s']
+    durations = [114, 182, 482]
+    plays = [0, 0, 0]
+    mute(driver, urls[0])
+    while(cont):
+        for i, url in enumerate(urls):
+            get_soup(driver, url)
+            time.sleep(durations[i])
+            plays[i] += 1
+            print ("done with link " + str(i+1))
+            if i == 2:
+                print(plays)
+           
